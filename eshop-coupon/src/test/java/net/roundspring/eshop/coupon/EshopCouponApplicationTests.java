30f36d7cd5ea685592bfb69510bb0d2895ecb513
Dawei Li <fmri2008@gmail.com>
2021-06-06 16:11:32 -0700

    added skeleton of all microservices

---
+++ eshop-coupon/src/test/java/net/roundspring/eshop/coupon/EshopCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package net.roundspring.eshop.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ eshop-ware/src/test/java/net/roundspring/eshop/ware/EshopWareApplicationTests.java
@@ -0,0 +1,13 @@
+package net.roundspring.eshop.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
