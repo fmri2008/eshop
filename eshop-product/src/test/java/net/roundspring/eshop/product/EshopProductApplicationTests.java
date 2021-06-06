83dcee8a069a6f32c38d0b1cc09e3b2d1538350a
Dawei Li <fmri2008@gmail.com>
2021-06-06 15:37:24 -0700

    added product microservice

---
+++ eshop-product/src/test/java/net/roundspring/eshop/product/EshopProductApplicationTests.java
@@ -0,0 +1,13 @@
+package net.roundspring.eshop.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
